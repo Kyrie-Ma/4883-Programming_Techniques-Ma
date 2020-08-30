98be788367264046a933379c13da98a3cd4a3f46
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-08-30 00:43:17 -0500

    main.cpp

---
+++ Assignment/11498/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int  main(){
+    int K, N, M, X, Y;
+    cin >> K;
+    while(K!=0){
+        cin >> N >> M;
+        for(int i = 0; i < K; i++){
+            cin >> X >> Y;
+            if(X == N || Y == M){
+                cout << "divisa\n";
+            }
+            else if(X > N && Y > M){
+                cout << "NE\n";
+            }
+            else if(X > N && Y < M){
+                cout << "SE\n";
+            }
+            else if(X < N && Y > M){
+                cout << "NO\n";
+            }
+            else if(X < N && Y < M){
+                cout << "SO\n";
+            }
+        }
+        cin >> K;
+    }
+}
