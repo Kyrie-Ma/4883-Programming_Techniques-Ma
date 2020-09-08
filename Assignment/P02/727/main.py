e64ebbfbdbd50deb7ec2c7ae62400246d1b6a722
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-08 12:15:36 -0500

    text

--- Assignment/P02/727/main.py
+++ Assignment/P02/727/main.py
@@ -3,7 +3,19 @@ import sys
 n = input()
 for i in range(int(n)):
     temp = input()
+    stack = []
+    for p 
     if temp != '(' or temp != ')' or temp != '+' or temp != '-' or temp != '*' or temp != '/':
         print(temp, end = '')
+        temp = input()
+        if temp == "\n":
+            break
     else:
-        if
+        if temp == '(':
+            stack.append(temp)
+            temp = input()
+        if temp == ')':
+            a = -2
+            while stack[a] != '(':
+                b = a+1
+                print(stack[b],  
