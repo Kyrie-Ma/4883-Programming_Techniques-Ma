5c43edd36f8a7b1e236772be46dced67a4d43319
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-08 04:08:35 -0500

    home

---
+++ Assignment/P02/673/main.py
@@ -0,0 +1,36 @@
+import sys
+
+#n = input()
+f = open("infile.txt", "r")
+n = int(f.readline())
+for i in range(int(n)):
+    #parentheses = input()
+    parentheses = f.readline().strip()
+
+    if len(parentheses) % 2 == 1:
+        print("No")
+
+    myStack = []
+
+    temp = "Yes"
+    for p in parentheses:
+        if p == '(' or p == '[':
+            myStack.append(p)
+        else:
+            if len(myStack) == 0:
+                print("No")
+                break
+            if p == ')':
+                if myStack[-1] == '(':
+                    myStack.pop()
+                else:
+                    print("No")
+                    break
+            if p == ']':
+                if myStack[-1] == '[':
+                    myStack.pop()
+                else:
+                    print("No")
+                    break
+    if len(myStack) > 0:
+        print("No")
