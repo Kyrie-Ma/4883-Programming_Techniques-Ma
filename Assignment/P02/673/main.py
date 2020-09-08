498f3be709e99dd75264e4dd36033f45bc2d31da
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-08 04:14:33 -0500

    home

--- Assignment/P02/673/main.py
+++ Assignment/P02/673/main.py
@@ -1,14 +1,15 @@
 import sys
 
-#n = input()
-f = open("infile.txt", "r")
-n = int(f.readline())
+n = input()
+#f = open("infile.txt", "r")
+#n = int(f.readline())
 for i in range(int(n)):
-    #parentheses = input()
-    parentheses = f.readline().strip()
+    parentheses = input()
+    #parentheses = f.readline().strip()
 
     if len(parentheses) % 2 == 1:
         print("No")
+        continue
 
     myStack = []
 
@@ -18,19 +19,23 @@ for i in range(int(n)):
             myStack.append(p)
         else:
             if len(myStack) == 0:
-                print("No")
+                #print("No")
+                temp = "No"
                 break
             if p == ')':
                 if myStack[-1] == '(':
                     myStack.pop()
                 else:
-                    print("No")
+                    #print("No")
+                    temp = "No"
                     break
             if p == ']':
                 if myStack[-1] == '[':
                     myStack.pop()
                 else:
-                    print("No")
+                    #print("No")
+                    temp = "No"
                     break
     if len(myStack) > 0:
         print("No")
+    print(temp)
