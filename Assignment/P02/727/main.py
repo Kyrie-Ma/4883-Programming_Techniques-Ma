c3270a25a5299741a26121c5c8347d6117b56010
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-09 15:44:14 -0500

    home

--- Assignment/P02/727/main.py
+++ Assignment/P02/727/main.py
@@ -3,8 +3,8 @@ import sys
 n = input()
 for i in range(int(n)):
     temp = input()
+    temp = input()
     stack = []
-    for p 
     if temp != '(' or temp != ')' or temp != '+' or temp != '-' or temp != '*' or temp != '/':
         print(temp, end = '')
         temp = input()
@@ -14,11 +14,50 @@ for i in range(int(n)):
         if temp == '(':
             stack.append(temp)
             temp = input()
+            if temp == '\n':
+                break
         if temp == ')':
             a = -2
             while stack[a] != '(':
                 b = a+1
                 print(stack[b])
-                stack.pop()
-                a--;
+                temp2 = stack.pop()
+                print(temp2, end = '')
+                a = a-1
             stack.pop()
+            temp = input()
+            if temp == '\n':
+                break
+        if temp == '*':
+            stack.append(temp)
+            temp = input()
+            if temp =='\n':
+                break
+        if temp == '/':
+            stack.append(temp)
+            temp = input()
+            if temp == '\n':
+                break
+        if temp == '+':
+            if stack[-1] == '*' or stack[-1] == '/':
+                c = -2
+                while stack[c] !='(':
+                    temp2 = stack.pop()
+                    print(temp2, end = '')
+            stack.append(temp)
+            temp = input()
+            if temp == '\n':
+                break
+        if temp == '-':
+            if stack[-1] == '*' or stack[-1] == '/':
+                c = -2
+                while stack[c] !='(':
+                    temp2 = stack.pop()
+                    print(temp2, end = '')
+            stack.append(temp)
+            temp = input()
+            if temp == '\n':
+                while stack.size() != 0:
+                    temp2 = stack.pop()
+                    print(temp2, end = '')
+                break
