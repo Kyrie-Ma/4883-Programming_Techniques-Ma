41837c6843a161506f6e93391953970f87c415b6
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-10 23:53:11 -0500

    Update main.cpp

--- Assignment/P02/514/main.cpp
+++ Assignment/P02/514/main.cpp
@@ -1,49 +1,45 @@
 
-#include <iostream>
-#include <vector>
-#include <stack>
+#include<cstdio>
+#include<stack>
+ 
 using namespace std;
+ 
+int N, c;
+stack<int> cars;
+ 
+void marshal() {
+    for(;;) {
+        while(cars.size() > 0) cars.pop();
+        int j = 0;
+        for(int i = 0; i < N; i++) {
+            scanf("%d", &c);
+            if(c == 0) return;
+ 
+            while(j < N && j != c) {
+                if(cars.size() > 0) {
+                  break;
+                }
+                if(cars.top() == c) {
+                  cars.pop();
+                }
+                j++;
+                cars.push(j);
+            }
 
-int main() {
-  int N;
-  int A[1001];
-  int train[1001];
-  stack<int> station;
-  while(cin >> N){
-    if (N == 0){
-      break;
-    }
-    while(cin>> train[0]){
-      if (train[0] == 0){
-        break;
-      }
-      while(!station.empty()){
-        station.pop();
-      }
-
-      for(int i = 1; i<N; i++){
-        cin>> train[i];
-      }
-
-      for(int j = 1; j <= N; j++){
-        station.push(j);
-        if(station.top()==train[j]){
-          station.pop();
         }
-      }
-      if(!station.empty()){
-        cout << "No\n";
-      }
-      else if(station.empty()){
-        cout << "Yes\n";
-      }
-      while(!station.empty()){
-        station.pop();
-      }
-
+        if(cars.size() == 0) printf("Yes\n");
+        else printf("No\n");
     }
-    int train[1001];
-    stack<int> station;
-  }
-  return 0;
+}
+ 
+int main() {
+    //for(;;) {
+        scanf("%d", &N);
+        if(N != 0){
+                  marshal();
+        }
+ 
+        //marshal();
+        printf("\n");
+    //}
 }
