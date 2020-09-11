b1c064e553a3bae6785805234f61e70680ff4839
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-11 05:37:01 -0500

    Update main.py

--- Assignment/P02/673/main.py
+++ Assignment/P02/673/main.py
@@ -1,41 +1,31 @@
-import sys
-
 n = input()
-#f = open("infile.txt", "r")
-#n = int(f.readline())
 for i in range(int(n)):
-    parentheses = input()
-    #parentheses = f.readline().strip()
-
-    if len(parentheses) % 2 == 1:
-        print("No")
-        continue
-
-    myStack = []
-
-    temp = "Yes"
-    for p in parentheses:
-        if p == '(' or p == '[':
-            myStack.append(p)
+  parentheses = input()
+  if len(parentheses) % 2 == 1:
+    print("No")
+    continue
+  myStack = []
+  temp = "Yes"
+  for p in parentheses:
+    if p == '(' or p == '[':
+      myStack.append(p)
+    else:
+      if len(myStack) == 0:
+        temp = "No"
+        break
+      if p == ')':
+        if myStack[-1] == '(':
+          myStack.pop()
         else:
-            if len(myStack) == 0:
-                #print("No")
-                temp = "No"
-                break
-            if p == ')':
-                if myStack[-1] == '(':
-                    myStack.pop()
-                else:
-                    #print("No")
-                    temp = "No"
-                    break
-            if p == ']':
-                if myStack[-1] == '[':
-                    myStack.pop()
-                else:
-                    #print("No")
-                    temp = "No"
-                    break
-    if len(myStack) > 0:
-        print("No")
-    print(temp)
+          temp = "No"
+          break
+      if p == ']':
+        if myStack[-1] == '[':
+          myStack.pop()
+        else:
+          temp = "No"
+          break
+  if len(myStack) > 0:
+    temp = "No"
+  myStack = []
+  print(temp)
