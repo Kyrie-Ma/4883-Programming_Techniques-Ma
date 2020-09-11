48d8e61038747308e2e727456aa51ab1b8485f23
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-11 05:00:12 -0500

    Update main.cpp

--- Assignment/P02/514/main.cpp
+++ Assignment/P02/514/main.cpp
@@ -1,45 +1,47 @@
-
-#include<cstdio>
+#include<iostream>
 #include<stack>
- 
+#include<queue>
 using namespace std;
- 
-int N, c;
-stack<int> cars;
- 
-void marshal() {
-    for(;;) {
-        while(cars.size() > 0) cars.pop();
-        int j = 0;
-        for(int i = 0; i < N; i++) {
-            scanf("%d", &c);
-            if(c == 0) return;
- 
-            while(j < N && j != c) {
-                if(cars.size() > 0) {
-                  break;
-                }
-                if(cars.top() == c) {
-                  cars.pop();
-                }
-                j++;
-                cars.push(j);
-            }
 
-        }
-        if(cars.size() == 0) printf("Yes\n");
-        else printf("No\n");
+void marshal(int N) {
+  int train;
+  stack<int> station;
+  for(;;) {
+    while(station.size() > 0) {
+      station.pop();
     }
-}
- 
-int main() {
-    //for(;;) {
-        scanf("%d", &N);
-        if(N != 0){
-                  marshal();
+    int A = 0;
+    for(int i = 0; i < N; i++) {
+      scanf("%d", &train);
+      if(train == 0) {
+        return;
+      }
+      while(A < N && A != train) {
+        if(station.size() > 0 && station.top() == train) {
+          break;
         }
- 
-        //marshal();
-        printf("\n");
-    //}
+        A++;
+        station.push(A);
+      }
+      if(station.top() == train) {
+        station.pop();
+      }
+    }
+    if(station.size() == 0) {
+      printf("Yes\n");
+    }
+    else 
+      printf("No\n");
+  }
+}
+int main() {
+  int N;
+  for(;;) {
+    scanf("%d", &N);
+    if(N == 0) {
+      break;
+    }
+    marshal(N);
+    printf("\n");
+  }
 }
