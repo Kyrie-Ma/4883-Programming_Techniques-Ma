edf9277fb405888c5ace2ebe02e5e97feb7ec82b
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-11 21:15:33 -0500

    Update main2.cpp

--- Assignment/P02/127/main2.cpp
+++ Assignment/P02/127/main2.cpp
@@ -1,1 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+using namespace std;
 
+int main() {
+  string card;
+  cin >> card;
+  
+  while (card != "#"){
+    vector<stack<string>> Pile(52);
+    Pile[0].push(card);
+
+    for (int i = 1; i < Pile.size(); i++){
+      cin >> card;
+      Pile[i].push(card);
+    }
+
+    for (int j = 0; j < Pile.size(); j++){
+      if (j >= 3 && (Pile[j].top().at(0) == Pile[j-3].top().at(0) || Pile[j].top().at(1) == Pile[j-3].top().at(1))){
+        Pile[j-3].push(Pile[j].top());
+        Pile[j].pop();
+        if (Pile[j].empty())
+        {
+          Pile.erase(Pile.begin() + j);
+        }
+        if(j == 3){
+          j = 0;
+        }
+        else
+          j = j-4;
+      }
+
+      if (j >= 1 && (Pile[j].top().at(0) == Pile[j-1].top().at(0) || Pile[j].top().at(1) == Pile[j-1].top().at(1))){
+        Pile[j-1].push(Pile[j].top());
+        Pile[j].pop();
+        if (Pile[j].empty())
+        {
+          Pile.erase(Pile.begin() + j);
+        }
+        if(j == 1){
+          j = 0;
+        }
+        else
+          j = j-2;
+      }
+    }
+    if(Pile.size() == 1){
+      cout << Pile.size() << " pile remaining: ";
+    }
+    else
+      cout << Pile.size() << " piles remaining: ";
+    
+    for(int z = 0; z < Pile.size(); z++){
+      if(z+1 == Pile.size()){
+        cout << Pile[z].size();
+      }
+      else
+        cout << Pile[z].size() << ' ';
+    }
+    cout << "\n";
+    cin >> card;
+  }
+  return 0;
+}
