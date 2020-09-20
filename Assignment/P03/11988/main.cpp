3949a1166c9e0645c6b5a4eaaea64acb8125ff55
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-09-20 06:24:26 -0500

    home

---
+++ Assignment/P03/11988/main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int main(){
+    char str[100000];
+    queue <char> temp;
+    while (scanf("%s", str) != EOF) {
+        int len = strlen(str);
+        for(int i = len; i >= 0; i--){
+            if(str[i] == '['){
+                for(int j = i+1; str[j] != ']' && str[j] != '[' && j < len; j++){
+                    temp.push(str[j]);
+                }
+            }
+        }
+        for(int i = 0; str[i] != '[' && str[i] != ']' && i < len; i++){
+            temp.push(str[i]);
+        }
+        for(int i = 0; i < len; i++){
+            if(str[i]==']'){
+                for(int j = i+1; str[j] != ']' && str[j] != '[' && j < len; j++){
+                    temp.push(str[j]);
+                }
+            }
+        }
+        int SIZE = temp.size();
+        for(int i = 0; i < SIZE; i++){
+            cout << temp.front();
+            temp.pop();
+        }
+        cout << endl;
+    }
+    return 0;
+}
