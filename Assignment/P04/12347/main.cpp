8227709ff3cc1efef6ba12f335c52570afacb1bb
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-10-09 06:10:18 -0500

    Update main.cpp

--- Assignment/P04/12347/main.cpp
+++ Assignment/P04/12347/main.cpp
@@ -2,57 +2,61 @@
 #include <vector>
 #include <fstream>
 using namespace std;
+
 struct node {
-    int value;
-    node *left, *right;
-    node() {
-        left = NULL;
-        right = NULL;
-    }
-    node(int temp) {
-        value = temp;
-        left = NULL;
-        right = NULL;
-    }
+  int value;
+  node *left, *right;
+  node() {
+    left = NULL;
+    right = NULL;
+  }
+  node(int temp) {
+    value = temp;
+    left = NULL;
+    right = NULL;
+  }
 };
 node *root = NULL;
+void add(int temp);
+void RecurPrint(node *current);
+
+int main() {
+  int treeValue;
+  while(scanf("%d", &treeValue)!=EOF){
+    add(treeValue);
+  }
+  RecurPrint(root);
+}
+
 void add(int temp) {
-    if(root==NULL) {
-        root = new node(temp);
-        return;
+  if(root==NULL) {
+    root = new node(temp);
+    return;
+  }
+  node *current = root, *parent;
+  while(current!=NULL) {
+    parent = current;
+    if(current->value<temp){
+      current = current->right;
     }
-    node *current = root, *parent;
-    while(current!=NULL) {
-        parent = current;
-        if(current->value<temp){
-          current = current->right;
-        }
-        else if(current->value>temp){
-          current = current->left;    
-        }
-        else 
-          return;
-    }
-    if(parent->value<temp) {
-        parent->right = new node(temp);
+    else if(current->value>temp){
+      current = current->left;    
     }
     else 
-        parent->left = new node(temp);
+      return;
+  }
+  if(parent->value<temp) {
+    parent->right = new node(temp);
+  }
+  else 
+    parent->left = new node(temp);
 }
 
 void RecurPrint(node *current) {
-    if(current==NULL){
-      return;
-    }
-    RecurPrint(current->left);
-    RecurPrint(current->right);
-    printf("%d\n", current->value);
-}
-
-int main() {
-    int treeValue;
-    while(scanf("%d", &treeValue)!=EOF){
-        add(treeValue);
-    }
-    RecurPrint(root);
+  if(current==NULL){
+    return;
+  }
+  RecurPrint(current->left);
+  RecurPrint(current->right);
+  printf("%d\n", current->value);
 }
