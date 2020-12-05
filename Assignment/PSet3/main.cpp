270f90427bdd89c8415073316c780c3b43d065f5
Kyrie-Ma <60235679+Kyrie-Ma@users.noreply.github.com>
2020-12-05 02:16:42 -0600

    asd

--- Assignment/PSet3/main.cpp
+++ Assignment/PSet3/main.cpp
@@ -9,30 +9,31 @@ int main() {
     int L1, L2;
     int count = 0;
     cin >> L1;
-    int train[L1-1];
+    int train[51];
     for(int j = 0;j< L1;j++){
       cin >> L2;
       //cout << L2 << " ";
-      train[i] = L2;
+      train[j] = L2;
       //cout << train[i] << endl;
     }
     for (int z = 0; z < L1; z++)
     {
       int temp = -1;
-      cout << " z: " << z << " \n";
-      if((z < L1) && (train[z+1] < train[z])){
+      //cout << " z: " << z << " \n";
+      if((z < L1-1) && (train[z+1] < train[z])){
+        //cout << "Trainz " << train[z] << " Trainz1 " << train[z+1] <<endl;
         temp = train[z];
         train[z] = train[z+1];
         train[z+1] = temp;
         count++;
-        z = 0;
-        cout << "temp " << temp <<endl; 
-        cout << "trainz " << train[z] << " trainz1 " << train[z+1] <<endl;
+        //cout << "temp " << temp <<endl; 
+        //cout << "trainz " << train[z] << " trainz1 " << train[z+1] <<endl;
+        z = -1;
         //cout << z << endl;
       }
 
     }
-    cout << "Optimal train swapping takes " << count << "swaps.\n";
+    cout << "Optimal train swapping takes " << count << " swaps.\n";
   }
   return 0;
 }
